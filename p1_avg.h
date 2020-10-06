2404e9c27bcd983dd2fd818745fde7cda7c4cc6e
jrpasillas <69991329+jrpasillas@users.noreply.github.com>
2020-10-05 20:12:33 -0600

    Update p1_avg.h

--- p1_avg.h
+++ p1_avg.h
@@ -2,13 +2,15 @@
 
 int main(void) {
   int n;
-  
+  //Read the number of numbers in the input
   scanf("%d", &n);
+  //If there are 3 set of numbers present, the average of the 3 numbers will be printed as output
   if(n == 3) {
     double x1, x2, x3;
     scanf("%lf %lf %lf", &x1, &x2, &x3);
     printf("%.4lf\n", (x1+x2+x3) / n);
   }
+  //This test finds the average of the numbers if there are 4 set of numbers present instead of 3
   else if (n == 4) {
     double x1, x2, x3, x4;
     scanf("%lf %lf %lf %lf", &x1, &x2, &x3, &x4);
